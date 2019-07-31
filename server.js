990ab39088bdd397d2605cc9c27229d8942ed448
ludwindl <deleonantigua@gmail.com>
2019-07-31 14:35:56 -0700

    returns an object

--- server.js
+++ server.js
@@ -8,20 +8,32 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-// app.get('/hello', (request, response) => {
-//   response.status(200).send('Hello, Test');
-// });
-
-app.get('/location', (request, response) => {
+//create a route with a method of get and a path of location
+app.get('/location', (request, response) =>{
   try{
-    let locationData = require ('./data/geo.json');
+    let locationData = getData(request.query.data);
     response.send(locationData);
   } catch(error) {
-    console.log('There was an error!')
+    console.log('There was an error!');
   }
-  response.status(200).send('locationData');
 });
 
-app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+//function to be invoked by the get() 
+
+function getData (locationName){
+  let jsonData = require('./data/geo.json');
+  let location = new Location(locationName, jsonData);
+  return location;
+}
+
+// function constructor
+function Location (query, jsonData) {
+  this.sear_query = query;
+  this.formatted_query = jsonData.results[0].formatted_address,
+  this.latitude = jsonData.results[0].geometry.location.lat,
+  this.longitude = jsonData.results[0].geometry.location.lng
+}
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+
+
