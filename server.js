469031a5bd9ebb3ce510b78b7900884d1c5fb762
James Portune <Jayms.Portune@gmail.com>
2019-07-30 21:00:53 -0700

    Saving Progress, got server working

--- server.js
+++ server.js
@@ -6,19 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
-
 app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
+  response.status(200).send('Hello, Test');
 });
 
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained'
-  }
-  response.status(200).json(airplanes);
+app.get('/location', (request, response) => {
+  response.status(200).send('Hello');
 });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
